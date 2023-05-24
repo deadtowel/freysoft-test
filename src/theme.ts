2e4440b26fc7f38f8a330bff6736804e255bb540
Pavlo Shabak <properfraction66@yahoo.com>
2023-05-25 02:27:53 +0300

    Set up custom theme

---
+++ src/theme.ts
@@ -0,0 +1,12 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#FF643B'
+    },
+    secondary: {
+      main: '#8234F8',
+    }
+  },
+});
